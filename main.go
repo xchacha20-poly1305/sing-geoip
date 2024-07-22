1ae8675fcbb05e28dde56c7223ebf048bf695ed8
HystericalDragon <HystericalDragons@proton.me>
2024-07-22 17:58:43 +0800

    Fix release
    Signed-off-by: HystericalDragon <HystericalDragons@proton.me>

--- main.go
+++ main.go
@@ -172,7 +172,7 @@ func release(source string, destination string, output string, ruleSetOutput str
 	if err != nil {
 		log.Warn("missing destination latest release")
 	} else {
-		if os.Getenv("NO_SKIP") != "true" && strings.Contains(*destinationRelease.Name, *sourceRelease.Name) {
+		if os.Getenv("NO_SKIP") != "true" && !strings.Contains(*destinationRelease.Name, *sourceRelease.Name) {
 			log.Info("already latest")
 			setActionOutput("skip", "true")
 			return nil
